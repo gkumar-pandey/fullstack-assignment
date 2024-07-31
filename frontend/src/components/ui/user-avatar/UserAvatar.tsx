3cad3af33e0b52c6ab20f35c88d1f7f64da50e55
gkumar-pandey <gautamshekhar078@gmail.com>
2024-07-31 14:05:50 +0530

    feat: user avatar component

---
+++ frontend/src/components/ui/user-avatar/UserAvatar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserAvatar = () => {
+  return (
+    <div className="flex flex-row items-center gap-2">
+      {/* <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026302d" /> */}
+      <p className="text-xl font-semibold text-[#080808]">John Doe</p>
+    </div>
+  );
+};
+
+export default UserAvatar;
