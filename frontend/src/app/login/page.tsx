9d839ef4c8c5e8829d042368f8e2a9d98e9d758e
gkumar-pandey <gautamshekhar078@gmail.com>
2024-07-31 13:42:21 +0530

    login page created

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import LoginForm from "@/components/core/LoginForm";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="bg-gradient min-h-dvh flex items-center justify-center ">
+      <div>
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
