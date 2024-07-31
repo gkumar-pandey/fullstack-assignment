ac3cd732c7d09ff68c2a61804b35a285437d3c12
gkumar-pandey <gautamshekhar078@gmail.com>
2024-07-31 13:43:42 +0530

    style:form heading component

---
+++ frontend/src/components/ui/formheading/FormHeading.tsx
@@ -0,0 +1,5 @@
+export const FormHeading = () => (
+  <h2 className="text-[#2D2D2D] text-5xl w-full text-center font-semibold ">
+    Welcome to <span className="text-[#4534AC]">Workflow</span>!
+  </h2>
+);
