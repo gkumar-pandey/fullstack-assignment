6634329f009b99b65df9e869c8d17c1fe4894706
gkumar-pandey <gautamshekhar078@gmail.com>
2024-07-31 13:42:36 +0530

    signup page created

---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+import SignupForm from "@/components/core/SignupForm";
+import React from "react";
+
+const SignupPage = () => {
+  return (
+    <div className="bg-gradient min-h-dvh flex items-center justify-center ">
+      <div>
+        <SignupForm />
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
